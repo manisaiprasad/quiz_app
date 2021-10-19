be17beccf360209f747c6e137137e611605cadf0
manisaiprasadam <manisaiprasadam@gmail.com>
2021-10-19 19:22:12 +0530

    Added login and signup views

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,35 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/',checkAuthenticated, function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+router.route('/signup')
+  .get(function(req, res) {
+    res.render('signup')
+  })
+
+router.route('/login')
+  .get(function(req, res) {
+    res.render('login')
+  })
+
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
+  
+  res.redirect('/login')
+}
+  
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/')
+    }
+  next()
+}
 
 module.exports = router;
