f548b812b877c19250923c48f996bb1a0c31c7bb
manisaiprasadam <manisaiprasadam@gmail.com>
2021-10-26 22:23:41 +0530

    added bcrypt and added passport

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const flash = require('express-flash');
+const session = require('express-session');
+const passport = require('passport')
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const bcrypt = require('bcrypt');
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use(flash());
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(logger('dev'));
 app.use(express.json());
