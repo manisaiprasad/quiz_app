7d7ea8d849d41074433edb9c25f1ef5cb728e312
manisaiprasadam <manisaiprasadam@gmail.com>
2021-11-22 14:58:00 +0530

    removed unnecessary prefix

--- migrations/20211111114324_tables_created.js
+++ migrations/20211111114324_tables_created.js
@@ -9,20 +9,20 @@ exports.up = async (knex) => {
     })
     .createTable('quiz', function (table) {
         table.increments('id');
-        table.string('quiz_name', 255).notNullable();
-        table.string('quiz_desc', 255).notNullable();
+        table.string('name', 255).notNullable();
+        table.string('desc', 255).notNullable();
         table.timestamp('created_at').defaultTo(knex.fn.now());
-        table.integer('quiz_created_by').unsigned().references('id').inTable('users');
-        table.string('quiz_level', 255).notNullable();
+        table.integer('created_by').unsigned().references('id').inTable('users');
+        table.string('level', 255).notNullable();
         table.integer('number_of_questions', 255).notNullable();
-        table.string('quiz_category', 255).notNullable();
-        table.string('quiz_time', 255);
-        table.string('quiz_pass_score', 255).notNullable();
+        table.string('category', 255).notNullable();
+        table.string('time', 255);
+        table.string('pass_score', 255).notNullable();
      })
      .createTable('quiz_questions', function (table) {
-        table.increments('question_id');
+        table.increments('id');
         table.string('question', 255).notNullable();
-        table.string('question_type',255);
+        table.string('type',255);
         table.string('option1', 255).notNullable();
         table.string('option2', 255).notNullable();
         table.string('option3', 255);
@@ -31,7 +31,7 @@ exports.up = async (knex) => {
         table.integer('quiz_id').unsigned().references('id').inTable('quiz');
      })
      .createTable('quiz_results', function (table) {
-        table.increments('result_id');
+        table.increments('id');
         table.string('result', 255).notNullable();
         table.integer('user_id').unsigned().references('id').inTable('users');
         table.integer('quiz_id').unsigned().references('id').inTable('quiz');
