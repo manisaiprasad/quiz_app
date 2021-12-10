03d1c60616cd80dac001ee161435a8df44d681d6
manisaiprasadam <manisaiprasadam@gmail.com>
2021-12-10 23:16:40 +0530

    imgration update

--- migrations/20211111114324_tables_created.js
+++ migrations/20211111114324_tables_created.js
@@ -2,32 +2,31 @@ exports.up = async (knex) => {
     return knex.schema
     .createTable('users', function (table) {
        table.increments('id');
-       table.string('full_name', 255).notNullable();
-       table.string('user_name', 255).notNullable();
-       table.string('email', 255).notNullable();
-       table.string('password', 255).notNullable();
+       table.string('full_name', 25).notNullable();
+       table.string('user_name', 25).notNullable();
+       table.string('email', 25).notNullable();
+       table.string('password', 25).notNullable();
     })
     .createTable('quiz', function (table) {
-        table.increments('id');
-        table.string('name', 255).notNullable();
-        table.string('desc', 255).notNullable();
+        table.increments('quiz_id');
+        table.string('name', 25).notNullable();
+        table.string('desc', 100).notNullable();
         table.timestamp('created_at').defaultTo(knex.fn.now());
         table.integer('created_by').unsigned().references('id').inTable('users');
-        table.string('level', 255).notNullable();
-        table.integer('number_of_questions', 255).notNullable();
-        table.string('category', 255).notNullable();
-        table.string('time', 255);
-        table.string('pass_score', 255).notNullable();
+        table.string('level', 15).notNullable();
+        table.integer('number_of_questions', 25).notNullable();
+        table.string('category', 25).notNullable();
+        table.string('pass_score', 25).notNullable();
      })
      .createTable('quiz_questions', function (table) {
         table.increments('question_id');
-        table.string('question', 255).notNullable();
-        table.string('type',255);
-        table.string('option1', 255).notNullable();
-        table.string('option2', 255).notNullable();
-        table.string('option3', 255);
-        table.string('option4', 255);
-        table.string('answer', 255).notNullable();
+        table.string('question', 100).notNullable();
+        table.string('type',25);
+        table.string('option1', 25).notNullable();
+        table.string('option2', 25).notNullable();
+        table.string('option3', 25);
+        table.string('option4', 25);
+        table.string('answer', 25).notNullable();
         table.integer('quiz_id').unsigned().references('id').inTable('quiz');
      })
      .createTable('quiz_results', function (table) {
