86377c5a15ba783d6b2c83669d87f0f18eecdcd3
manisaiprasadam <manisaiprasadam@gmail.com>
2021-12-13 16:33:53 +0530

    attached paginate

--- configs/db-config.js
+++ configs/db-config.js
@@ -3,5 +3,6 @@ const knex = require('knex');
 const config = require('../knexfile.js');
 
 const db = knex(config.development);
-
+const { attachPaginate } = require('knex-paginate');
+attachPaginate();
 module.exports = db;
