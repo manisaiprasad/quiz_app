37f5f89092ebd3bdf9b377e44032b99e9192c66a
manisaiprasadam <manisaiprasadam@gmail.com>
2021-12-19 01:53:00 +0530

    configured database connection from dot env

--- knexfile.js
+++ knexfile.js
@@ -2,13 +2,13 @@
 
 module.exports = {
   development: {
-    client: 'mysql',
+    client: process.env.DB_CLIENT,
     connection: {
-      host : '127.0.0.1',
+      host : process.env.DB_HOST,
       port : 3306,
-      user : 'root',
-      password : '12345678',
-      database : 'quiz'
+      user : process.env.DB_USER,
+      password : process.env.DB_PASSWORD,
+      database : process.env.DB_DATABASE
     },
     migrations: {
       tableName: 'migrations'
